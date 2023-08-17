997582d2d68f8a45333691916337bd4e840a08b9
Pedro Augusto <132590212+PedroAugusto16@users.noreply.github.com>
2023-08-17 15:49:43 -0300

    Add files via upload

--- jogo.js
+++ jogo.js
@@ -1,5 +1,9 @@
 
 
+var altura = 0
+var largura = 0
+var vidas = 1
+
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
@@ -12,9 +16,15 @@ ajustaTamanhoPalcoJogo()
 function posicaoRandomica() {
 
     if(
-        document.getElementById('mosquito')
-    ){
+        document.getElementById('mosquito')){
         document.getElementById('mosquito').remove()
+    }
+
+    if(vidas>3){
+        location.href='fim_de_jogo.html'
+    }else{
+        document.getElementById('v'+vidas).src = 'imagens/coracao_vazio.png'
+        vidas++
     }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -33,7 +43,7 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
-
+    mosquito.onclick = function(){this.remove()}
     document.body.appendChild(mosquito)
     console.log(ladoAleatorio())
 }
